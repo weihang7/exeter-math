weihang7/exeter-math

commit 96ac30c403542859e8b79bc4164e566a90ef540b

team name

static/build/grading.js
@@ -1,7 +1,7 @@
 (function() {
-  var check, checkboxes, cur_scores, grade, guts_round, guts_round_control, id, id_control, map, password, password_control, refresh, refresh_guts, round, serialize, submit, validate, verify;
+  var check, checkboxes, cur_scores, grade, guts_round, guts_round_control, id, id_control, map, password_control, refresh, refresh_guts, round, serialize, submit, uassword, validate, verify;
 
-  password = $('#password');
+  uassword = $('#password');
 
   round = $('#round');
 
@@ -109,11 +109,13 @@
       for (i = _i = 1, _ref = map[round.val()]; 1 <= _ref ? _i <= _ref : _i >= _ref; i = 1 <= _ref ? ++_i : --_i) {
         checkboxes.append($("<div class='checkbox'> <label>" + i + "<input type='checkbox'></label> </div>"));
       }
-      return guts_round_control.addClass('hidden');
+      guts_round_control.addClass('hidden');
     } else {
       guts_round_control.removeClass('hidden');
-      return refresh_guts();
+      refresh_guts();
     }
+    ($('#graded')).text('');
+    return cur_scores = [];
   };
 
   refresh_guts = function() {
@@ -138,12 +140,10 @@
       success: function(data) {
         var scores;
         scores = JSON.parse(data.scores);
-        if (scores.length > 0) {
+        if (scores && scores.length > 0) {
           ($('#graded')).text(data.name);
           cur_scores = scores;
           return validate();
-        } else {
-          return ($('#graded')).text('');
         }
       }
     });

static/src/script/grading.coffee
@@ -1,4 +1,4 @@
-password = $ '#password'
+uassword = $ '#password'
 round = $ '#round'
 id = $ '#id'
 guts_round = $ '#guts-round'
@@ -80,6 +80,8 @@ refresh = ->
     else
         guts_round_control.removeClass 'hidden'
         refresh_guts()
+    ($ '#graded').text ''
+    cur_scores = []
 
 refresh_guts = ->
     checkboxes.empty()
@@ -99,12 +101,10 @@ check = ->
         }
         success: (data) ->
             scores = JSON.parse(data.scores)
-            if scores.length > 0
+            if scores and scores.length > 0
                 ($ '#graded').text data.name
                 cur_scores = scores
                 validate()
-            else
-                ($ '#graded').text ''
 
 refresh()
 round.change refresh

handlers/team.py
@@ -380,7 +380,7 @@ class CheckHandler(BaseHandler):
     def get(self):
         rnd = self.request.get('round')
         _id = self.request.get('id')
-        name = 'No Face'
+        name = ''
         ret = '[]'
         if rnd in ('speed', 'accuracy'):
             ind = Individual.query(Individual.assigned_id == _id)

commit d9a8ebfb539224df64537553de39ff39cda133de

Fix inexplicable typo that was introduced in 96ac30c

static/build/grading.js
@@ -1,7 +1,7 @@
 (function() {
-  var check, checkboxes, cur_scores, grade, guts_round, guts_round_control, id, id_control, map, password_control, refresh, refresh_guts, round, serialize, submit, uassword, validate, verify;
+  var check, checkboxes, cur_scores, grade, guts_round, guts_round_control, id, id_control, map, password, password_control, refresh, refresh_guts, round, serialize, submit, validate, verify;
 
-  uassword = $('#password');
+  password = $('#password');
 
   round = $('#round');
 

static/grading.html
@@ -104,6 +104,6 @@
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="sha512.js"></script>
-    <script src="build/grading.min.js"></script>
+    <script src="build/grading.js"></script>
   </body>
 </html>

static/src/grading.jade
@@ -59,4 +59,4 @@ html(lang='en')
     script(src='//code.jquery.com/jquery-1.11.0.min.js')
     script(src='//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js')
     script(src='sha512.js')
-    script(src='build/grading.min.js')
+    script(src='build/grading.js')

static/src/script/grading.coffee
@@ -1,4 +1,4 @@
-uassword = $ '#password'
+password = $ '#password'
 round = $ '#round'
 id = $ '#id'
 guts_round = $ '#guts-round'

static/src/script/list_scores.coffee
@@ -2,10 +2,10 @@ teams_list = $ '#teams_list'
 individuals_list = $ '#individuals_list'
 
 # TODO get real weights
-SPEED = 2
-ACCURACY = 10
-TEAM = 13
-GUTS = 15
+SPEED = 3
+ACCURACY = 9
+TEAM = 20
+GUTS = [3,3,3,5,5,5,7,7,7,9,9,9,11,11,11,13,13,13,15,15,15,18,18,18,22,22,22]
 
 reload = (sortfun) ->
     $.ajax
@@ -58,6 +58,10 @@ formatScore = (arr) ->
         return "n/a"
 
 formatGuts = (arr) ->
+    total = 0
+    if arr?
+        total += GUTS[i] for el, i in arr when el
+    return total
 
 getScore = (arr) ->
     count = 0